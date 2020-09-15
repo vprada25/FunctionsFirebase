c0ff09babd5a2f9b5da09a263555eac0c56acf03
victor <v.prada@udla.edu.co>
2020-09-15 06:38:28 -0500

    refactor: config server firebase and views

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const path = require('path');
+
+admin.initializeApp();
+
+// app.set('port',  4000);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//routes
+
+//app.use(require(path.join(__dirname, './src/routes/index')));
+app.use(require('./src/routes/index'));
+
+/*app.get('/',(req,res)=>{
+    res.render("./src/views/index.hbs");
+});*/
+
+app.set('views', path.join(__dirname, './src/views'));
+
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    partialsDir: path.join(__dirname, './src/views/layouts/partials'),
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+exports.app=functions.https.onRequest(app);
--- functions/src/routes/index.js
+++ functions/src/routes/index.js
@@ -1,37 +1,26 @@
 const { Router } = require('express');
 const router = Router();
 
-
-
-
-
 router.get('/', (req, res) => {
-    res.render('../views/index.hbs')
+    console.log('holaaaa')
+    res.render('index.hbs')
 })
 
 router.get('/about', (req, res) => {
-    res.render('../views/layouts/about-us/about.hbs')
+    res.render('./layouts/about-us/about.hbs')
 })
 
 router.get('/login', (req, res) => {
-    res.render('../views/layouts/login/login.hbs')
+    res.render('./layouts/login/login.hbs')
 })
 
 router.get('/register', (req, res) => {
-    res.render('../views/layouts/register/register.hbs')
+    res.render('./layouts/register/register.hbs')
 })
 
 router.get('/admin', (req, res) => {
-    res.render('../views/layouts/admin/admin.hbs')
+    res.render('./layouts/admin/admin.hbs')
 })
-
-
-
-
-
-
-
-
 
 
 module.exports = router;
